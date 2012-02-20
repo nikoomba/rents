82289c218b9c0ebe8b7ed68784b74cb01d3cd95f
Daniel Jones <hack@riseup.net>
2012-02-20 20:24:27 +1100

    explanatory notes about how to use in the README

--- rents.py
+++ rents.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main_page():
-    return "Hello World! Change me later!"
+    return "Hello World! Change me later! This mainpage will just be a simple sort of search box"
 
 @app.route("/<loc>")
 def location_view(loc):
@@ -23,7 +23,7 @@ def geocode(addr):
     location = pythonified_json['results'][0]['geometry']['location'] #stored as dict. To get lat go location['lat']
     return location
 
-#geocode("Glasgow") #note '%20' only needs to be in for testing purposes since the browser will automatically change a space into '%20' when sending the request to the handler. 
+#geocode("Glasgow") #note '%20' only needs to be in for testing purposes since the browser will automatically change a space in to '%20' when sending the request to the handler. 
 
 """Run Flask if this file is executed by python. Useful to comment this out for testing in the commandline, or else uncomment the 'app.debug=True' line"""
 
