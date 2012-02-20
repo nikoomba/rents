22e7a327ec3ef48600e2b05247f98d3849c4abf3
Daniel Jones <hack@riseup.net>
2012-02-20 20:09:57 +1100

    basic geocoding being served to the browser

--- rents.py
+++ rents.py
@@ -3,15 +3,17 @@ import urllib2
 try: import simplejson as json
 except ImportError: import json
 
-rents = Flask(__name__)
+app = Flask(__name__)
 
-@rents.route("/")
+@app.route("/")
 def main_page():
-    pass
+    return "Hello World! Change me later!"
 
-@rents.route("/<location>")
-def location_view(location):
-    return geocode(location)
+@app.route("/<loc>")
+def location_view(loc):
+    x = geocode(loc)
+    (lat,lng)=(x['lat'],x['lng'])
+    return "Lat: {0}, Long: {1}".format(lat,lng)
 
 def geocode(addr):
     """ Fetch geocoded location. Currently we are using the google maps geocoding api located at  http://code.google.com/apis/maps/documentation/geocoding/ It only allows 2500 requests a day. An option to get around this later is to embed this request client-side or perhaps to explore the use of OpenStreetMap?"""
@@ -21,8 +23,11 @@ def geocode(addr):
     location = pythonified_json['results'][0]['geometry']['location'] #stored as dict. To get lat go location['lat']
     return location
 
-geocode("Glasgow%20University") #note '%20' only needs to be in for testing purposes since the browser will automatically change a space into '%20' when sending the request to the handler. 
+#geocode("Glasgow") #note '%20' only needs to be in for testing purposes since the browser will automatically change a space into '%20' when sending the request to the handler. 
 
-"""Run Flask if this file is executed by python. Useful to comment this out for testing"""
+"""Run Flask if this file is executed by python. Useful to comment this out for testing in the commandline, or else uncomment the 'app.debug=True' line"""
+
 if __name__ == '__main__':
+    app.debug = True
     app.run()
+
